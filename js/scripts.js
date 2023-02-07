d4f5929644183c69ad2c8ca86acea7c0069d197f
vladliena <sotnikovavladlena@gmail.com>
2023-02-07 15:25:58 +0200

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,125 +1,217 @@
-const shopPage = document.getElementById('shop')
+const shop = document.getElementById('shop')
 
-const shoes = document.getElementsByClassName('shoes_link')[0];
-shoes.addEventListener('click', shoesOpenList);
+const arr = [
+    {
+        title: 'Phones',
+        items: [
+            {
+                title: 'Iphone',
+                items: [
+                    {
+                        title: 'Iphone 13',
+                        description: '6.1-inch screen. Super Retina XDR display is 28% brighter up to 800 nits for regular content 1200 nits for HDR.',
+                    }
+                ]
+            },
+            {
+                title: 'Sumsung Galaxy',
+                items: [
+                    {
+                        title: 'Sumsung Galaxy S22',
+                        description: '5.75 x 2.78 x 0.30 inches (146 x 70.6 x 7.6mm). Back: Glass (Corning Gorilla Glass Victus); Frame: Aluminum',
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        title: 'Laptops',
+        items: [
+            {
+                title: "Acer",
+                items: [
+                    {
+                        title: 'Acer Nitro 5',
+                        description: 'Windows 10 Home 64-bit · Intel® Core™ i5-10300H processor Quad-core 2.50 GHz · NVIDIA® GeForce RTX™ 3050 with 4 GB dedicated memory'
+                    }
+                ]
+            },
+            {
+                title: "Macbook",
+                items: [
+                    {
+                        title: 'Acer Nitro 5',
+                        description: 'Display. Retina display. 13.3-inch (diagonal) LED-backlit display with IPS technology; 2560-by-1600 native resolution at 227 pixels per inch'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        title: 'Speakers',
+        items: [
+            {
+                title: "Marshall",
+                items: [
+                    {
+                        title: 'Marshall Stanmore',
+                        description: 'True Stereophonic technology, which is essentially a multi-directional sound technology. This enables the speaker to provide excellent 360-degree sounds, regardless of the speaker\'s placement.'
+                    }
+                ]
+            },
+            {
+                title: "Bose",
+                items: [
+                    {
+                        title: 'Bose Soundlink',
+                        description: 'Meet SoundLink Revolve II, one of our best performing portable Bluetooth speakers yet. It delivers true 360° sound for consistent, uniform coverage.'
+                    }
+                ]
+            }
+        ]
+    },
+];
 
-function shoesOpenList() {
-    const shoesVariation = document.createElement('div');
-    shoesVariation.className = 'shoes_variation';
-    shoesVariation.innerHTML = '<ul><li><a href="#" class="bootsInfo">boots</a></li><li><a href="#" class="trinersInfo">trainers</a></li>'
-    while (shopPage.children[1]) shopPage.children[1].remove();
-    shopPage.appendChild(shoesVariation)
-    const bootsInfo = document.getElementsByClassName('bootsInfo')[0];
-    const trinersInfo = document.getElementsByClassName('trinersInfo')[0]
-    bootsInfo.addEventListener('click', function (){
-        const bootsCard = document.createElement('article');
-        bootsCard.className = 'bootsCard';
-        bootsCard.innerHTML = '<h2>Leather Boots</h2><br><ul><li>Real Leather</li><li>Sizes 36-39</li><li>New Collection</li></ul>'
-        while (shopPage.children[2]) shopPage.children[2].remove();
-        shopPage.appendChild(bootsCard)
-        const buyButton = document.createElement('button');
-        buyButton.className = 'buyProductButton';
-        buyButton.setAttribute('type', 'button');
-        buyButton.appendChild(document.createTextNode('Buy'))
-        bootsCard.appendChild(buyButton);
-        buyButton.addEventListener('click', purchased)
-    });
-    trinersInfo.addEventListener('click', function (){
-        const trainersCard = document.createElement('article');
-        trainersCard.className = 'bootsCard';
-        trainersCard.innerHTML = '<h2>Adidas Trainers</h2><br><ul><li>Low-profile design</li><li>Sizes 36-39</li><li>Flame print to sides</li></ul>'
-        while (shopPage.children[2]) shopPage.children[2].remove();
-        shopPage.appendChild(trainersCard)
-        const buyButton = document.createElement('button');
-        buyButton.className = 'buyProductButton';
-        buyButton.setAttribute('type', 'button');
-        buyButton.appendChild(document.createTextNode('Buy'))
-        trainersCard.appendChild(buyButton);
-        buyButton.addEventListener('click', purchased)
-    })
-}
-
-const accessories = document.getElementsByClassName('accessories_link')[0];
-accessories.addEventListener('click', openAccessoriesList);
-function openAccessoriesList (){
-    const accessoriesVariation = document.createElement('div');
-    accessoriesVariation.className = 'accessoriesVariation'
-    accessoriesVariation.innerHTML = '<ul><li><a href="#" class="ringInfo">ring</a></li><li><a href="#" class="braceletInfo">bracelet</a></li></ul>'
-    while (shopPage.children[1]) shopPage.children[1].remove();
-    shopPage.appendChild(accessoriesVariation);
-    const ringInfo = document.getElementsByClassName('ringInfo')[0];
-    const braceletInfo = document.getElementsByClassName('braceletInfo')[0]
-    ringInfo.addEventListener('click', function (){
-        const ringCard = document.createElement('article');
-        ringCard.className = 'ringCard';
-        ringCard.innerHTML = '<h2>Gold Ring</h2><br><ul><li>With diamond</li><li>Size 36.5</li></ul>'
-        while (shopPage.children[2]) shopPage.children[2].remove();
-        shopPage.appendChild(ringCard)
-        const buyButton = document.createElement('button');
-        buyButton.className = 'buyProductButton';
-        buyButton.setAttribute('type', 'button');
-        buyButton.appendChild(document.createTextNode('Buy'))
-        ringCard.appendChild(buyButton);
-        buyButton.addEventListener('click', purchased)
-    });
-    braceletInfo.addEventListener('click', function (){
-        const braceletCard = document.createElement('article');
-        braceletCard.className = 'braceletCard';
-        braceletCard.innerHTML = '<h2>Silver Bracelet</h2><br><ul><li>Your sign can be added</li><li>One Size</li></ul>'
-        while (shopPage.children[2]) shopPage.children[2].remove();
-        shopPage.appendChild(braceletCard)
-        const buyButton = document.createElement('button');
-        buyButton.className = 'buyProductButton';
-        buyButton.setAttribute('type', 'button');
-        buyButton.appendChild(document.createTextNode('Buy'))
-        braceletCard.appendChild(buyButton);
-        buyButton.addEventListener('click', purchased)
-    })
-}
-
-
-const faceBody = document.getElementsByClassName('face_body_link')[0];
-faceBody.addEventListener('click', openFaceBodyList);
-
-function openFaceBodyList (){
-    const faceBodyVariation = document.createElement('div');
-    faceBodyVariation.className = 'faceBodyVariation'
-    faceBodyVariation.innerHTML = '<ul><li><a href="#" class="showerGelInfo">shower gel</a></li><li><a href="#" class="bodyCreamInfo">body cream</a></li></ul>'
-    while (shopPage.children[1]) shopPage.children[1].remove();
-    shopPage.appendChild(faceBodyVariation);
-    const showerGelInfo = document.getElementsByClassName('showerGelInfo')[0];
-    const bodyCreamInfo = document.getElementsByClassName('bodyCreamInfo')[0]
-    showerGelInfo.addEventListener('click', function (){
-        const showerGelCard = document.createElement('article');
-        showerGelCard.className = 'showerGelCard';
-        showerGelCard.innerHTML = '<h2>Apple Shower Gel</h2><br><ul><li>For sensitive skin</li><li>organic</li></ul>'
-        while (shopPage.children[2]) shopPage.children[2].remove();
-        shopPage.appendChild(showerGelCard)
-        const buyButton = document.createElement('button');
-        buyButton.className = 'buyProductButton';
-        buyButton.setAttribute('type', 'button');
-        buyButton.appendChild(document.createTextNode('Buy'))
-        showerGelCard.appendChild(buyButton);
-        buyButton.addEventListener('click', purchased)
-    });
-    bodyCreamInfo.addEventListener('click', function (){
-        const bodyCreamCard = document.createElement('article');
-        bodyCreamCard.className = 'bodyCreamCard';
-        bodyCreamCard.innerHTML = '<h2>Nivea Body Cream</h2><br><ul><li>Delicate softener</li><li>organic product</li></ul>'
-        while (shopPage.children[2]) shopPage.children[2].remove();
-        shopPage.appendChild(bodyCreamCard)
-        const buyButton = document.createElement('button');
-        buyButton.className = 'buyProductButton';
-        buyButton.setAttribute('type', 'button');
-        buyButton.appendChild(document.createTextNode('Buy'))
-        bodyCreamCard.appendChild(buyButton);
-        buyButton.addEventListener('click', purchased)
-    })
-}
-
-function purchased (){
-    if(alert('Thank you for your purchase!')){
-    } else {
-        window.location.reload();
+function categories(arr) {
+    const categoriesUl = document.createElement('ul');
+    for (const key of arr) {
+        const categoriesLi = document.createElement('li');
+        categoriesLi.classList.add('categories')
+        categoriesLi.innerHTML = key.title
+        categoriesUl.appendChild(categoriesLi)
     }
+    shop.appendChild(categoriesUl)
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const categoriesList = document.querySelectorAll('.categories')
+    categoriesList.forEach((element) => {
+        element.addEventListener('click', () => {
+            const buyButton = document.createElement('button');
+            buyButton.setAttribute('type','click');
+            buyButton.appendChild(document.createTextNode('Buy'));
+            buyButton.className = 'buyProductButton';
+            buyButton.addEventListener('click',function (){
+                    if(alert('Thank you for your purchase!')){
+                    } else {
+                        window.location.reload();
+                    }
+            });
+            if (element.innerHTML === 'Phones') {
+                for (const i of arr) {
+                    if (i.title === 'Phones') {
+                        const phonesUl = document.createElement('ul');
+                        const items = i.items;
+                        for (const g of items) {
+                            const phonesName = document.createElement('li');
+                            phonesName.innerHTML += g.title;
+                            phonesName.classList.add('phonesName');
+                            phonesUl.appendChild(phonesName);
+                            phonesName.addEventListener('click', () => {
+                                const phoneDescriptionUl = document.createElement('ul');
+                                if (phonesName.innerHTML === 'Iphone') {
+                                    for (let i = 0; i < g.items.length; i++) {
+                                        const iphoneCard = document.createElement('li');
+                                        iphoneCard.innerHTML += Object.values(g.items[i]).toString()
+                                        iphoneCard.classList.add('iphone_description');
+                                        phoneDescriptionUl.appendChild(iphoneCard);
+                                    }
+                                }
+                                else if (phonesName.innerHTML === 'Sumsung Galaxy'){
+                                    for (let i = 0; i < g.items.length; i++) {
+                                        const sumsungCard = document.createElement('li');
+                                        sumsungCard.innerHTML += Object.values(g.items[i]).toString()
+                                        sumsungCard.classList.add('galaxy_description');
+                                        phoneDescriptionUl.appendChild(sumsungCard);
+                                    }
+                                }
+                                while (shop.children[2]) shop.children[2].remove()
+                                shop.appendChild(phoneDescriptionUl)
+                                phoneDescriptionUl.appendChild(buyButton)
+                            })
+                            }
+                            while (shop.children[1]) shop.children[1].remove()
+                            shop.appendChild(phonesUl)
+                        }
+                    }
+                }
+            else
+                if (element.innerHTML === 'Laptops') {
+                    for (const i of arr) {
+                        if (i.title === 'Laptops') {
+                            const laptopsUl = document.createElement('ul');
+                            const items = i.items;
+                            for (const g of items) {
+                                const laptopsName = document.createElement('li');
+                                laptopsName.innerHTML += g.title;
+                                laptopsName.classList.add('laptopsName');
+                                laptopsUl.appendChild(laptopsName);
+                                laptopsName.addEventListener('click', () => {
+                                    const laptopDescriptionUl = document.createElement('ul');
+                                    if (laptopsName.innerHTML === 'Acer') {
+                                        for (let i = 0; i < g.items.length; i++) {
+                                            const acerCard = document.createElement('li');
+                                            acerCard.innerHTML += Object.values(g.items[i]).toString()
+                                            acerCard.classList.add('acer_description');
+                                            laptopDescriptionUl.appendChild(acerCard);
+                                        }
+                                    }
+                                    else if (laptopsName.innerHTML === 'Macbook'){
+                                        for (let i = 0; i < g.items.length; i++) {
+                                            const macbookCard = document.createElement('li');
+                                            macbookCard.innerHTML += Object.values(g.items[i]).toString()
+                                            macbookCard.classList.add('macbook_description');
+                                            laptopDescriptionUl.appendChild(macbookCard);
+                                        }
+                                    }
+                                    while (shop.children[2]) shop.children[2].remove()
+                                    shop.appendChild(laptopDescriptionUl)
+                                    laptopDescriptionUl.appendChild(buyButton)
+                                })
+                            }
+                            while (shop.children[1]) shop.children[1].remove()
+                            shop.appendChild(laptopsUl)
+                        }
+                    }
+                } else if (element.innerHTML === 'Speakers') {
+                    for (const i of arr) {
+                        if (i.title === 'Speakers') {
+                            const SpeakersUl = document.createElement('ul');
+                            const items = i.items;
+                            for (const g of items) {
+                                const speakersName = document.createElement('li');
+                                speakersName.innerHTML += g.title;
+                                speakersName.classList.add('speakersName');
+                                SpeakersUl.appendChild(speakersName);
+                                speakersName.addEventListener('click', () => {
+                                    const speakerDescriptionUl = document.createElement('ul');
+                                    if (speakersName.innerHTML === 'Marshall') {
+                                        for (let i = 0; i < g.items.length; i++) {
+                                            const marshallCard = document.createElement('li');
+                                            marshallCard.innerHTML += Object.values(g.items[i]).toString()
+                                            marshallCard.classList.add('marshall_description');
+                                            speakerDescriptionUl.appendChild(marshallCard);
+                                        }
+                                    }
+                                    else if (speakersName.innerHTML === 'Bose'){
+                                        for (let i = 0; i < g.items.length; i++) {
+                                            const boseCard = document.createElement('li');
+                                            boseCard.innerHTML += Object.values(g.items[i]).toString()
+                                            boseCard.classList.add('bose_description');
+                                            speakerDescriptionUl.appendChild(boseCard);
+                                        }
+                                    }
+                                    while (shop.children[2]) shop.children[2].remove()
+                                    shop.appendChild(speakerDescriptionUl)
+                                    speakerDescriptionUl.appendChild(buyButton)
+                                })
+                            }
+                            while (shop.children[1]) shop.children[1].remove()
+                            shop.appendChild(SpeakersUl)
+                        }
+                    }
+                }
+            });
+        });
+    })
+    categories(arr)
